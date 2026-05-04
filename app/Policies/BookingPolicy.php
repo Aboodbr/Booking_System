f238c00fe2517c0226812f3708398b9115c6b4e4
Aboodbr <aboodragon12345@gmail.com>
2026-05-04 10:30:42 +0300

    edit policy based on admin only

--- app/Policies/BookingPolicy.php
+++ app/Policies/BookingPolicy.php
@@ -4,63 +4,44 @@ namespace App\Policies;
 
 use App\Models\Booking;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class BookingPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
     public function viewAny(User $user): bool
     {
-        return false;
+        return true; // الكل يقدر يشوف القائمة
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
     public function view(User $user, Booking $booking): bool
     {
-        return $user->id === $booking->user_id;
+        return $user->role === 'admin'
+            || $user->id === $booking->user_id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): bool
     {
-        return false;
+        return $user->role === 'admin' || $user->role === 'user';
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, Booking $booking): bool
     {
-        return false;
+        return $user->role === 'admin'
+            || $user->id === $booking->user_id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, Booking $booking): bool
     {
-        return false;
+        return $user->role === 'admin'
+            || $user->id === $booking->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
     public function restore(User $user, Booking $booking): bool
     {
-        return false;
+        return $user->role === 'admin';
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
     public function forceDelete(User $user, Booking $booking): bool
     {
-        return false;
+        return $user->role === 'admin';
     }
 }
